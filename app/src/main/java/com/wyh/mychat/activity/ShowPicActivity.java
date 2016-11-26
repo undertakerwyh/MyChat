73c5287ec90ab439f9259a21de4a92bd05a1b469
trundles <undertakerwyh@sina.cn>
2016-11-26 16:33:35 +0800

    集成环信sdk初步

---
+++ app/src/main/java/com/wyh/mychat/activity/ShowPicActivity.java
@@ -0,0 +1,15 @@
+package com.wyh.mychat.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.wyh.mychat.R;
+
+public class ShowPicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_pic);
+    }
+}
