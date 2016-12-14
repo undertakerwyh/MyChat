9a37ddecc020be303c4ba395e3087d79dd6782ca
Administrator <undertakerwyh@sina.cn>
2016-12-14 15:21:15 +0800

    消息下拉修复

--- app/src/main/java/com/wyh/mychat/activity/TalkActivity.java
+++ app/src/main/java/com/wyh/mychat/activity/TalkActivity.java
@@ -70,6 +70,7 @@ public class TalkActivity extends BaseActivity implements View.OnClickListener, 
                         if (endIndex == startIndex) {
                             lvTalkMessage.setSelection(0);
                         } else {
+                            Log.e("AAA","lvTalkMessage.isInTouchMode()"+lvTalkMessage.isInTouchMode());
                             lvTalkMessage.setSelection(endIndex - startIndex + 1);
                         }
                     }
